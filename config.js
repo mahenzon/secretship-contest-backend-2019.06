4e6b0d1958fa87f04c8b6b7557445ed1a0423b22
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-15 13:43:58 +0300

    Create config file

---
+++ config.js
@@ -0,0 +1,22 @@
+const { resolve } = require('path')
+const { randomFillSync } = require('crypto')
+const { existsSync, readFileSync, writeFile } = require('fs')
+
+function readSecret () {
+  const secretPath = resolve(process.env.SECRET_FILEPATH || 'sessionSecret')
+  if (existsSync(secretPath)) {
+    return readFileSync(secretPath, 'utf8')
+  }
+  const secret = randomFillSync(Buffer.alloc(42)).toString('hex')
+  writeFile(secretPath, secret, (err) => !err || console.log(err))
+  return secret
+}
+
+module.exports = {
+  token: process.env.BOT_TOKEN,
+  domain: process.env.DOMAIN,
+  hookPath: process.env.WEBHOOK_PATH || null,
+  port: process.env.PORT || 3001,
+  mongoConnectUri: process.env.MONGO_URI || 'mongodb://localhost/contest-bot',
+  sessionSecret: process.env.SECRET || readSecret(),
+}
