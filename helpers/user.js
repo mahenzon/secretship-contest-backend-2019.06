99d22c214ea513177861a818d034b46e11264851
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-16 10:35:24 +0300

    Edit user helper Move helpers to a separate file from api

--- helpers/user.js
+++ helpers/user.js
@@ -1,10 +1,12 @@
+const checkIntegrity = require('../utils/tg-check-integrity')
 const User = require('../models/user')
+const {
+  sendError,
+  sendServerError,
+} = require('./api')
 
+const ONE_DAY = 1000 * 60 * 60 * 24
 
-function sendServerError(res, err) {
-  console.log('There was an error!:', err)
-  return res.status(500).json({ error: 'Error fetching database!' })
-}
 
 function preparedUser({
   createdAt,
@@ -34,14 +36,14 @@ function sendExistingUser(res, user) {
 
 function findAndSendUser(res, user_id) {
   if (Number.isNaN(Number(user_id))) {  // https://github.com/airbnb/javascript#standard-library
-    return res.status(400).json({ error: `ID cannot be like '${user_id}'!` })
+    return sendError(400, `ID cannot be like '${user_id}'!`, res)
   }
   User.findOne({ user_id }, (err, user) => {
     if (err) {
-      return sendServerError(res, err)
+      return sendServerError('Error fetching database!', res, err)
     }
     if (!user) {
-      return res.status(404).json({ error: `User with id ${user_id} not found` })
+      return sendError(404, `User with id ${user_id} not found`, res)
     }
     console.log('User is', user)
     return sendExistingUser(res, user)
@@ -52,16 +54,46 @@ function getAuthorizedUsers(res) {
   // TODO: offset, limit
   User.find({}, (err, users) => {
     if (err) {
-      return sendServerError(res, err)
+      return sendServerError('Error fetching database!', res, err)
     }
     return res.json({ users: users.map(preparedUser) })
   })
 }
 
+// Login
+async function loginUser(req, res, params) {
+  if (!checkIntegrity(params)) {
+    return sendError(401, 'Not Authorized', res)
+  }
+  const now = new Date()
+  const authDate = params.auth_date * 1000
+  if (now - authDate > ONE_DAY) {
+    return sendError(401, 'Session expired, please click login button again', res)
+  }
+
+  const user_id = params.id
+  req.session.userId = user_id  // mark as authorized
+  return findAndSendUser(res, user_id)
+}
+
+// Logout
+async function logoutUser(req, res) {
+  req.session.destroy((err) => {
+    if (err) {
+      return sendServerError('Error logging you out. Please try again', res, err)
+    }
+    res.clearCookie()
+    return res.json({})
+  })
+}
+
+
 const userHelper = {
   sendExistingUser,
   findAndSendUser,
   getAuthorizedUsers,
+  loginUser,
+  logoutUser,
 }
 
 module.exports = userHelper
--- routes/api.js
+++ routes/api.js
@@ -1,45 +1,20 @@
 const { Router } = require('express')
 
-const checkIntegrity = require('../utils/tg-check-integrity')
-const { findAndSendUser, getAuthorizedUsers } = require('../helpers/user')
+const {
+  findAndSendUser,
+  getAuthorizedUsers,
+  loginUser,
+  logoutUser,
+} = require('../helpers/user')
 
 const router = Router()
-const ONE_DAY = 1000 * 60 * 60 * 24
 
 
-async function login(req, res, params) {
-  if (!checkIntegrity(params)) {
-    return res.json({ error: 'Not Authorized' })
-  }
-  const now = new Date()
-  const authDate = params.auth_date * 1000
-  if (now - authDate > ONE_DAY) {
-    return res.json({ error: 'Session expired, please click login button again' })
-  }
+router.get('/login', async (req, res) => loginUser(req, res, req.query))
+router.post('/login', async (req, res) => loginUser(req, res, req.body))
 
-  const user_id = params.id
-  req.session.userId = user_id  // mark as authorized
-  return findAndSendUser(res, user_id)
-}
-
-router.get('/login', async (req, res) => login(req, res, req.query))
-router.post('/login', async (req, res) => login(req, res, req.body))
-
-
-async function logout(req, res) {
-  req.session.destroy((err) => {
-    if (err) {
-      console.log(err)
-      res.send('ERROR')
-      return
-    }
-    res.clearCookie()
-    res.send('SUCCESS LOGOUT')
-  })
-}
-
-router.get('/logout', logout)
-router.post('/logout', logout)
+router.get('/logout', logoutUser)
+router.post('/logout', logoutUser)
 
 
 // Get the logged in user
